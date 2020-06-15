0c47f48230f0416c788210e1946f8017a4d34d30
Ben <ben.pearce@octopus.com>
2020-06-15 16:14:10 +1000

    say hello twice

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace simplecoreapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
