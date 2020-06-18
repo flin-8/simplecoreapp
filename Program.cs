1633c2ad311a4f268eb263157b4ba2364685e02a
Frank Lin <frank.lin@octopus.com>
2020-06-19 08:43:01 +1000

    Remove changes

--- Program.cs
+++ Program.cs
@@ -4,14 +4,12 @@ namespace simplecoreapp
 {
     class Program
     {
-        //Change 2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Goodbye World!");
             Console.WriteLine("Goodbye World!");
-            //Change 1
         }
     }
 }
